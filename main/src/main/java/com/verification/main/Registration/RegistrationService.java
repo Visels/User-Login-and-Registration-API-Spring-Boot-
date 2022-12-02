739c76ea6f5fa125dd29de05001f4e544090e7c3
visels <elviskipchumba@yahoo.com>
2022-12-03 01:05:29 +0300

    web security configuration done

---
+++ main/src/main/java/com/verification/main/Registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package com.verification.main.Registration;
+
+public class RegistrationService {
+
+
+    public String register(RegistrationRequest request) {
+
+        return "works";
+    }
+}
